eb87352de32bebcc0ebf09786f3b512819bb3163
iamNargiz <61267849+iamNargiz@users.noreply.github.com>
2021-09-21 14:20:57 +0400

    Add files via upload

---
+++ PrimeChecking.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+num = int(input('Enter the number: '))
+
+
+# In[21]:
+
+
+
+# If given number is greater than 1
+if num > 1:
+ 
+    # Iterate from 2 to n / 2
+    for i in range(2, int(num/2)+1):
+ 
+        # If num is divisible by any number between
+        # 2 and n / 2, it is not prime
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+ 
+else:
+    print(num, "is not a prime number")
+
+
+# In[ ]:
+
+
+
+
